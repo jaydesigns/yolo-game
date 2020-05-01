393284229865568e2eb8fa278ca4aeab011b01fd
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2020-05-01 11:34:31 -1000

    Simplified game engine

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        var topicArr = [
-"A food seasoning",
-"A Star Wars character",
-"A country in the world",
-"Something Blue",
-"A Song artist",
-"Something you can touch",
-"A type of Chilli Pepper",
-"Name of a tree",
-"A boy's name",
-"Foods you eat for breakfast",
-"An animal",
-"Things that are cold",
-"Name of insects",
-"Name of TV Show",
-"Things that grow",
-"School subject",
-"A Vegetable",
-"Musical instruments",
-"Name of a Boyband",
-"Car manufacturer",
-"Clothing brand",
-"Items in a suitcase",
-"Halloween costume",
-"Things that are round"];
-var topic = document.getElementById("topic");
-var letter = document.getElementById("letter");
-var letterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-letter.innerText = letterArr[Math.floor(Math.random() * letterArr.length)];
-
-topic.innerText = topicArr[Math.floor(Math.random() * topicArr.length)];
+        chooseTopicAndLetter();
     }
 }
 
+function chooseTopicAndLetter(){
+    chooseTopic();
+    chooseLetter();
+}
+
 function chooseTopic(){
-
-
-var topicArr = [
+        var topicArr = [
 "A food seasoning",
-"A Star Wars character",
+"A movie character",
 "A country in the world",
 "Something Blue",
-"A Song artist",
+"A Song/Music artist",
 "Something you can touch",
-"A type of Chilli Pepper",
 "Name of a tree",
 "A boy's name",
-"Foods you eat for breakfast",
+"Food you eat for breakfast",
 "An animal",
 "Things that are cold",
 "Name of insects",
@@ -57,18 +28,26 @@ var topicArr = [
 "School subject",
 "A Vegetable",
 "Musical instruments",
-"Name of a Boyband",
 "Car manufacturer",
 "Clothing brand",
 "Items in a suitcase",
 "Halloween costume",
-"Things that are round"];
+"Things that are round",
+"Part of the body",
+"Fastfood restaurants",
+"An element in the periodic table",
+"A color",
+"A name of a city",
+"A pet's name"];
 var topic = document.getElementById("topic");
+topic.innerText = topicArr[Math.floor(Math.random() * topicArr.length)];
+    };
+        
+function chooseLetter(){
+        
 var letter = document.getElementById("letter");
 var letterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 letter.innerText = letterArr[Math.floor(Math.random() * letterArr.length)];
 
-topic.innerText = topicArr[Math.floor(Math.random() * topicArr.length)];
-
-}
+    };
